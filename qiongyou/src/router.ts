8a08b4961dc9d98b455ca47af640cc8bcba1d5e5
qiutu270 <dazhao2016831@gmail.com>
2019-06-13 00:07:27 +0800

    updata

--- qiongyou/src/router.ts
+++ qiongyou/src/router.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
+import loginpage from './views/loginpage.vue';
+import home from './views/Home.vue';
 
 Vue.use(Router);
 
@@ -10,16 +11,16 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home,
+      name: 'loginpage',
+      component: loginpage,
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/home',
+      name: 'home',
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+      component: () => import(/* webpackChunkName: "about" */ './views/Home.vue'),
     },
   ],
 });
--- qiongyou/src/views/Home.vue
+++ qiongyou/src/views/Home.vue
@@ -1,18 +1,3 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+    <h1>首页</h1>
 </template>
-
-<script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
-export default class Home extends Vue {}
-</script>
