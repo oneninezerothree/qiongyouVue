d29cd32e8167c0b2f80ecbe112eec21220a229fd
wzsharen123 <603410800@qq.com>
2019-06-16 18:39:29 +0800

    登录注册完成-大招

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">
+    <div id="nav">
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+   html,body{
+     font-size: 20px
+   }
+</style>
---
+++ src/views/loginpage.vue
@@ -0,0 +1,16 @@
+<template>
+   <div class="loginpage">
+      <loginIn/>
+   </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import loginIn from '@/components/loginIn.vue'
+@Component({
+  components: {
+    loginIn,
+  },
+})
+export default class Home extends Vue {}
+</script>
